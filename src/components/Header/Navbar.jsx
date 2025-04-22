edde292c980458535be52c359c8fd32e7b01a0f5
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-04-22 19:34:11 +0600

    complete book vibe

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,19 +1,15 @@
-import React, { useState } from "react";
-import { Link, useLocation } from "react-router";
+import React from "react";
+import { Link, useLocation } from "react-router"; 
 
 const Navbar = () => {
   const location = useLocation();
-  const [active, setActive] = useState(location.pathname);
-  const handleActive = (path) => {
-    setActive(path);
-  };
+  const currentPath = location.pathname; 
 
   const getLinkClass = (path) => {
-    const baseClass =
-      "text-lg font-semibold px-4 py-2 border rounded-lg ml-2";
+    const baseClass = "text-lg font-semibold px-4 py-2 border rounded-lg ml-2";
     const activeClass = "border-[#23BE0A] text-[#23BE0A]";
     const inactiveClass = "border-transparent hover:border-[#23BE0A] hover:text-[#23BE0A]";
-    return `${baseClass} ${active === path ? activeClass : inactiveClass}`;
+    return `${baseClass} ${currentPath === path ? activeClass : inactiveClass}`;
   };
 
   return (
@@ -32,32 +28,34 @@ const Navbar = () => {
             </svg>
           </div>
           <ul className="text-center menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-44 p-2 shadow">
-            <Link to="/" onClick={() => handleActive("/")}>
-              <li className={`${getLinkClass("/")} mb-2`}>Home</li>
-            </Link>
-            <Link to="/listedBooks" onClick={() => handleActive("/listedBooks")}>
-              <li className={`${getLinkClass("/listedBooks")} mb-2`}>Listed Books</li>
-            </Link>
-            <Link to="/pagesToRead" onClick={() => handleActive("/pagesToRead")}>
-              <li className={getLinkClass("/pagesToRead")}>Pages to Read</li>
-            </Link>
+          
+              <Link to="/" className={`${getLinkClass("/")} mb-2`}><li>Home</li></Link>
+           
+          
+              <Link to="/listedBooks" className={`${getLinkClass("/listedBooks")} mb-2`}><li>Listed Books</li></Link>
+          
+           
+              <Link to="/pagesToRead" className={`${getLinkClass("/pagesToRead")}`}><li>Pages to Read</li></Link>
+          
           </ul>
         </div>
         <a className="font-bold text-xl ml-2">Book Vibe</a>
       </div>
+
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <Link to="/" onClick={() => handleActive("/")}>
-            <li className={getLinkClass("/")}>Home</li>
-          </Link>
-          <Link to="/listedBooks" onClick={() => handleActive("/listedBooks")}>
-            <li className={getLinkClass("/listedBooks")}>Listed Books</li>
-          </Link>
-          <Link to="/pagesToRead" onClick={() => handleActive("/pagesToRead")}>
-            <li className={getLinkClass("/pagesToRead")}>Pages to Read</li>
-          </Link>
+          
+            <Link to="/" className={getLinkClass("/")}><li>Home</li></Link>
+          
+        
+            <Link to="/listedBooks" className={getLinkClass("/listedBooks")}><li>Listed Books</li></Link>
+        
+        
+            <Link to="/pagesToRead" className={getLinkClass("/pagesToRead")}><li>Pages to Read</li></Link>
+         
         </ul>
       </div>
+
       <div className="navbar-end">
         <button className="btn mr-2 btn-success text-white font-semibold p-3">Sign In</button>
         <button className="btn btn-accent text-white font-semibold p-3">Sign Up</button>
